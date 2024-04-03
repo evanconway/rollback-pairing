230d4ff62f40df9f10283e72d405f5b80125d850
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-04-03 19:52:11 -0400

    add basic chat connection

---
+++ phone-tutorial-app/script.js
@@ -0,0 +1,54 @@
+let conn;
+
+const peerId = `${Math.floor(Math.random() * 2 ** 18).toString(36).padStart(4, 0)}`;
+
+const peer = new Peer(peerId,
+    {
+      host: location.hostname,
+      debug: 1,
+      path: "/myapp",
+    },
+);
+
+window.peer = peer;
+
+document.getElementById('chatbox-form')?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const chatTextInput = document.getElementById('chatbox-textinput');
+    const pNode = document.createElement('p');
+    const message = chatTextInput.value;
+    pNode.innerHTML = message;
+    pNode.style.backgroundColor = 'white';
+    document.getElementById('chatbox')?.appendChild(pNode);
+    chatTextInput.value = '';
+    
+    if (conn.open) {
+        conn.send(message);
+    }
+});
+
+peer.on("open", () => {
+    window.caststatus.textContent = `Your device ID is: ${peer.id}`;
+});
+
+const setupConnection = (conn) => {
+    conn.on('data', (d) => {
+        console.log('data:', d);
+        const pNode = document.createElement('p');
+        pNode.innerHTML = d;
+        pNode.style.backgroundColor = 'yellow';
+        document.getElementById('chatbox')?.appendChild(pNode);
+    });
+};
+
+// click event for call button
+document.querySelector(".call-btn").addEventListener("click", () => {
+    const code = window.prompt("Please enter the sharing code");
+    conn = peer.connect(code);
+    setupConnection(conn);
+});
+
+peer.on("connection", (connection) => {
+    conn = connection;
+    setupConnection(conn);
+});
--- phone-tutorial-app/server.js
+++ phone-tutorial-app/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const { ExpressPeerServer } = require("peer");
+
 const app = express();
 const server = http.createServer(app);
-const { ExpressPeerServer } = require("peer");
 
 const port = process.env.PORT || "8000";
 
@@ -21,6 +22,10 @@ app.use(express.static(path.join(__dirname)));
 app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/index.html`);
 });
+
+// app.get("/manifest.json", (request, response) => {
+//   response.sendFile(`${__dirname}/manifest.json`);
+// });
 
 server.listen(port);
 console.log(`Listening on: ${port}`);
