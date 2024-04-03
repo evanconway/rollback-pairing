dd3c9c810bc581c1a7e50dafde6779f4c9dc464d
AceOfHeart5 <evanconwaymusic@gmail.com>
2024-04-03 16:54:07 -0400

    add text to blue box

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+console.log('hello world');
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const onFormSubmit = (e: SubmitEvent) => {
+	e.preventDefault();
+  
+  const submitted = (e.target as HTMLFormElement)?.elements[0].value;
+	console.log('sending text: ', submitted);
+  const pNode = document.createElement('p');
+  pNode.innerHTML = submitted;
+  document.getElementById('chatbox')?.appendChild(pNode);
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function main() {
+	document.getElementById('chatbox-form')?.addEventListener('submit', onFormSubmit);
+}
+
+main();
