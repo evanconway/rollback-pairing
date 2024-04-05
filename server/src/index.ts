6f2438c4688227b2f956f0c5cc1ef4b120fa8c7e
Evan Conway <evanconwaymusic@gmail.com>
2024-04-05 16:11:09 -0400

    server serves frontend files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,33 @@
-import express from 'express';
+import express from "express";
+import http from "http";
+import path from "path";
+import { ExpressPeerServer } from "peer";
 
 const app = express();
+const server = http.createServer(app);
 
-app.get('/', (req, res) => {
+const port = process.env.PORT || "8000";
+
+const peerServer = ExpressPeerServer(server, {
+  proxied: true,
+  //debug: true,
+  path: "/myapp",
+  //ssl: undefined,
+});
+
+app.use(peerServer);
+
+
+const staticFileDir = '../../client/dist';
+app.use(express.static(path.join(__dirname, staticFileDir)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, staticFileDir, '/index.html'));
+});
+
+app.get('hello', (req, res) => {
     res.send('hello world');
 });
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`app running at http://localhost:${port}`);
-});
+server.listen(port);
+console.log(`Listening on: ${port}`);
